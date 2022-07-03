d9719944e0de5b839e6db0c0df16888f80cab683
Carly Donais <carly.donais@gmail.com>
2022-07-03 17:03:43 -0400

    added foreign key constraints

--- schema.sql
+++ schema.sql
@@ -6,12 +6,14 @@ CREATE TABLE roles(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
-    department_id INTEGER
+    CONSTRAINT fk_departmentId FOREIGN KEY (id)
+    REFERENCES departments(id)
 );
 CREATE TABLE employees(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER, 
-    manager_id INTEGER
+    last_name VARCHAR(30) NOT NULL, 
+    manager_id INTEGER,
+    CONSTRAINT fk_roleId FOREIGN KEY (id)
+    REFERENCES roles(id)
 );
